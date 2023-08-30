1485ac20b6193eae42306e91f4885072937c7869
Kamal <kamal_dia@hotmail.com>
2023-08-30 19:55:37 +0300

    step_6 completed

---
+++ javascript-basics-01/step_6.js
@@ -0,0 +1,5 @@
+var first_number = prompt("Enter first number");
+var second_number = prompt("Enter second number");
+var division = first_number / second_number ; // step 4 said "4.Divide them"
+var remainder= first_number % second_number ;
+alert("The remainder of the division of the first number over the second is " + remainder)
