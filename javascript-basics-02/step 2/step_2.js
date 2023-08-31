0c0e6371128bb9467bbfd38fc7d42fdeb4b7d24a
Kamal <kamal_dia@hotmail.com>
2023-08-31 14:01:09 +0300

    step_2

---
+++ javascript-basics-02/step 2/step_2.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){
+  var name_1 = document.querySelector("#name");
+  name_1.addEventListener("blur", function() {alert("thank you for participating!")});
+});
