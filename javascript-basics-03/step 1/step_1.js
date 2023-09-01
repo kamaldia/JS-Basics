7844570daa61fbf6f5bf7cc7bf18216e4e1f11ad
Kamal <kamal_dia@hotmail.com>
2023-09-01 19:03:59 +0300

    step_1

---
+++ javascript-basics-03/step 1/step_1.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function (){
+  var image = document.querySelector("#image1");
+  document.addEventListener("mouseover", function (){
+    image.style.border = "3px solid red";
+  })
+})
