b15e8d1b8325c321b98f562ee551b10ea6307c7c
Kamal <kamal_dia@hotmail.com>
2023-09-01 16:51:02 +0300

    step_5

---
+++ javascript-basics-02/step 5/step_5.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+var images = document.querySelectorAll("img");
+for(let image in images){
+  images[image].addEventListener("mouseover",function(){
+    images[image].setAttribute("src",`images/image${+image+1}_2.jpg`);
+  }); 
+}})
